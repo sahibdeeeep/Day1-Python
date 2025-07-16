bcbbf526d53653ec7d9e7d376a342e059252647c
Sahibdeep Singh <sahibdeeeep@gmail.com>
2025-07-16 11:44:02 +0530

    Added Python files: FirstProgram and Variables

---
+++ FirstProgram.py
@@ -0,0 +1,6 @@
+print("hello world")
+print("sahib is my name." , "my age is 18")
+
+print(10+10)
+
+print("10+10")
---
+++ Variables.py
@@ -0,0 +1,17 @@
+name = "sahib"
+age = 18
+price = 24.99
+
+age2 = age
+
+print(name)
+print(age)
+print(price)
+
+print("my name is: ", name ,"my age :" , age2 )
+
+#or
+
+print("my name is: ", name )
+print("my age: ", age2)
+ 

24bf23587ee6f5965bc3a5949027af2d00401b1b
Sahibdeep Singh <sahibdeeeep@gmail.com>
2025-07-16 17:23:41 +0530

    update

--- Variables.py
+++ Variables.py
@@ -1,6 +1,8 @@
 name = "sahib"
 age = 18
 price = 24.99
+gf = False
+job = None
 
 age2 = age
 
@@ -10,13 +12,52 @@ print(price)
 
 print("my name is: ", name ,"my age :" , age2 )
 
+
 #or
+
 
 print("my name is: ", name )
 print("my age: ", age2)
 
+
 #Types
+
 
 print(type(name))
 print(type(age))
 print(type(price))
+print(type(gf))
+print(type(job))
+
+
+#sums
+
+
+a = 2
+b = 5
+sum = a + b
+print(sum)
+
+
+
+#INPUTS
+
+
+
+name = input("name : ")
+print(name)
+
+age = int(input("your age: "))
+print(age)
+
+price = float(input("what is pen price: "))
+print(price)
+
+#or
+
+
+name = input("confirm name : ")
+age = int(input("comfirm age: "))
+price = float(input("confirm pen price: "))
+
+print("my name is: " , name , "and my age is:" , age , ",my pen price is:" , "$" ,price)
